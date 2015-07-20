2cbd7576c12ff2ea3c5d2e04d1901ebef1bbcdcc
Kodlee Yin <kodlee@kodleeshare.net>
2015-07-19 21:21:27 -0400

    Added space for features

--- .site/php/runescape/pages/home.php
+++ .site/php/runescape/pages/home.php
@@ -9,40 +9,72 @@ $headerImages = array(
 		"https://s3.amazonaws.com/fm-runescape/home-headers/wwNBVGa.jpg",
 		"https://s3.amazonaws.com/fm-runescape/home-headers/ZLib4qc.jpg"
 );
+$randomImage = array_rand($headerImages);
 ?>
 
 <div class="home-header"
-	 style="background-image: url(<?php echo $headerImages[array_rand($headerImages)]; ?>)">
+	 style="background-image: url(<?php echo $headerImages[$randomImage]; ?>)">
 
 	<div class="title">
 		<h1>FM - RuneScape</h1>
 		<h2>A bunch of random tools for RS</h2>
 	</div>
 
-	<a class="button header-button" href="#home-login">Explore</a>
+	<div class="header-buttons">
+		<a class="button" href="#home-content">Explore</a>
+		<a class="button" href="#home-login">Login</a>
+	</div>
+
 </div>
 
-<div class="home-login" id="home-login">
-	<div class="aligner"></div>
-	<div class="left-half">
-		<div class="login-text">
-			You are logging into the Fru1tMe website -- NOT RUNESCAPE. Please do not type in your
-			RuneScape credentials. I don't want them. Jagex probably prefers you don't type them
-			here. In fact, YOU probably don't want to type them here anyway.
-		</div>
-	</div>
-	<div class="divider"><div></div></div>
-	<div class="right-half">
-		<form action="/" method="post">
-			<div class="form-text">Username</div>
-			<div class="form-input"><input type="text" name="fm-usrn" /></div>
-			<div class="form-spacer"></div>
-			<div class="form-text">Password</div>
-			<div class="form-input"><input type="password" name="fm-pswd" /></div>
-			<div class="form-spacer"></div>
-			<div class="form-spacer"></div>
+<div class="home-content" id="home-content">
 
-			<div class="form-input"><button type="submit">Login</button></div>
-		</form>
+</div>
+
+<div class="home-login"
+	 id="home-login"
+	 style="background-image: url(<?php echo $headerImages[$randomImage]; ?>)">
+	<div class="login-wrapper">
+		<div class="aligner"></div>
+		<div class="left-half">
+			<div class="login-text">
+				You are logging into the Fru1tMe website. Not RuneScape. Please don't try using
+				your RuneScape login here. It wont work. Seriously.
+			</div>
+		</div>
+		<div class="divider"><div></div></div>
+		<div class="right-half">
+			<form action="/" method="post">
+				<div class="form-box">
+					<div class="form-text">Username</div>
+					<div class="form-input">
+						<input type="text" name="fm-usrn" tabindex="1" />
+					</div>
+				</div>
+				<div class="form-box">
+					<div class="form-text">Password</div>
+					<div class="form-input">
+						<input type="password" name="fm-pswd" tabindex="2" />
+					</div>
+				</div>
+				<input type="checkbox" id="create-account" class="hidden" />
+				<div class="form-box">
+					<div class="form-text">Verify Password</div>
+					<div class="form-input">
+						<input type="password" name="fm-rp-pswd" tabindex="4"/>
+					</div>
+				</div>
+				<div class="form-box form-controls">
+					<div class="form-input">
+						<button type="submit" class="login-button" tabindex="3"></button>
+					</div>
+					<div class="form-input">
+						<label for="create-account" class="create-account-button button">
+							Create Account
+						</label>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
 </div>
